39b4ff6c020c1f013fc24873e6e093d9992ebdf7
Démontant <olivier@creativconflans.fr>
2025-09-29 03:32:31 +0200

    ✨ Ajout: Amélioration de la navigation et mise à jour de la page "Made in Conflans"
    ✅ Ajout de l'attribut `target="_blank"` aux liens dans `guest.vue` pour ouvrir les pages dans un nouvel onglet.
    🎨 Mise en forme et ajout de contenu à la page `made-in-conflans.vue`, incluant un titre, une description et un logo pour mieux présenter l'initiative locale.

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -9,18 +9,18 @@
           <Icon name="heroicons:camera" class="w-6 h-6 mb-1" />
           Ma photo
         </NuxtLink>
-        <NuxtLink to="/made-in-conflans"
+        <NuxtLink to="/made-in-conflans" target="_blank"
           class="flex flex-col items-center text-xs text-gray-700 hover:text-yellow-500 transition-colors">
           <Icon name="heroicons:sparkles" class="w-6 h-6 mb-1" />
           Made in Conflans
         </NuxtLink>
-        <NuxtLink to="/concours"
+        <NuxtLink to="/concours" target="_blank"
           class="flex flex-col items-center text-xs text-gray-700 hover:text-pink-600 transition-colors"
           active-class="text-pink-600 font-bold">
           <Icon name="heroicons:gift" class="w-6 h-6 mb-1" />
           Concours
         </NuxtLink>
-        <NuxtLink to="/acheter"
+        <NuxtLink to="/acheter" target="_blank"
           class="flex flex-col items-center text-xs text-gray-700 hover:text-green-600 transition-colors"
           active-class="text-green-600 font-bold">
           <Icon name="heroicons:shopping-cart" class="w-6 h-6 mb-1" />
--- pages/made-in-conflans.vue
+++ pages/made-in-conflans.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    
+    <div class="min-h-screen bg-[#f7f5f2] px-4 py-12">
+      <div class="max-w-4xl mx-auto flex flex-col items-center justify-center">
+        <img src="/logo-mic.svg" alt="Made in Conflans" class="w-24 mb-4" />
+        <h1 class="text-2xl font-bold text-gray-900">Made in Conflans</h1>
+        <p class="text-gray-900/70 text-center">Made in Conflans est une initiative de la mairie de Conflans-Sainte-Honorine pour promouvoir le territoire et les commerces locaux.</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
 </script>
-
+definePageMeta({
+  layout: 'auth',
+  title: 'Made in Conflans',
+})
 <style>
 
 </style>
